9c71e90cbe47bb21bfaecb157704a225d4f202be
mariamat285 <mariamat285@gmail.com>
2026-06-24 15:37:02 +0000

    Creation de la fonction chargerFilms

--- src/film.c
+++ src/film.c
@@ -1,52 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "film.h"
+//============FONCTION CHARGER FILMS=========================//
+Film *chargerFilms(char *nomFichier, int *nbFilms)
+{
 
-/*
- * Constructeur / destructeur */
+    FILE *fichier = fopen(nomFichier, "r");
 
-void init_catalogue(Catalogue *c) {
-    c->alloc = 0;
-    c->n = 0;
-    c->tab = NULL;
-}
-
-void clear_catalogue(Catalogue *c) {
-    free(c->tab);   
-    c->tab = NULL;
-    c->alloc = 0;
-    c->n = 0;
-}
-
-/** Sauvegarde du catalogue (réécriture complète du fichier)*/
-
-int sauvegarder_catalogue(const Catalogue *c, const char *nomFichier) {
-    FILE *f = fopen(nomFichier, "w"); 
-    if (f == NULL) {
-        fprintf(stderr, "Erreur : impossible d'écrire dans %s\n", nomFichier);
-        return 0;
+    if (fichier == NULL)
+    {
+        *nbFilms = 0;
+        return NULL;
     }
 
-    for (int i = 0; i < c->n; i++) {
-        fprintf(f, "%d;%s;%s;%d;%.1f\n",
-                c->tab[i].id,
-                c->tab[i].titre,
-                c->tab[i].genre,
-                c->tab[i].annee,
-                c->tab[i].note);
+    int compteur = 0;
+    char ligne[256];
+
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL)
+    {
+        compteur++;
     }
 
-    fclose(f);
-    return 1;
+    Film *tableau = malloc(compteur * sizeof(Film));
+
+    if (tableau == NULL)
+    {
+        fclose(fichier);
+        *nbFilms = 0;
+        return NULL;
+    }
+
+    rewind(fichier);
+
+    int i = 0;
+
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL)
+    {
+        if (ligne[0] == '\n' || ligne[0] == '\0')
+        continue;
+
+        char *token;
+
+        token = strtok(ligne, ";");
+        tableau[i].id = atoi(token);
+
+        token = strtok(NULL, ";");
+        strcpy(tableau[i].titre, token);
+
+        token = strtok(NULL, ";");
+        strcpy(tableau[i].genre, token);
+
+        token = strtok(NULL, ";");
+        tableau[i].annee = atoi(token);
+
+        token = strtok(NULL, ";");
+        tableau[i].note = atof(token);
+
+        i++;
+    }
+
+    fclose(fichier);   
+
+    *nbFilms = i;
+
+    return tableau;
 }
-
-
-
-/* void ajouter_film(Catalogue *c) { ... } */
-/* void afficher_films(const Catalogue *c) { ... } */
-/* void rechercher_par_id(const Catalogue *c, int id) { ... } */
-/* void rechercher_par_titre(const Catalogue *c, const char *motif) { ... } */
-/* void afficher_par_genre(const Catalogue *c, const char *genre) { ... } */
-/* void modifier_film(Catalogue *c, int id) { ... } */
-/* void supprimer_film(Catalogue *c, int id) { ... } */
